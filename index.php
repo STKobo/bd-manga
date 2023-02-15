e59791ec1f5dc5500880db4c8b97bd703d5fdcc8
Stephane_tcha <stephanetcha@gmail.com>
2023-02-15 20:01:09 +0100

    initialisation du projet

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Product {
+    public int $name; 
+    public string $description;
+    public float $price; 
+
+    public function getPrice(){
+
+    }
+}
+
+
+$product1 = new Product();
